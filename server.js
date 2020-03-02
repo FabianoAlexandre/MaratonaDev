31f7b67f29b8e707dd3a35590f023eac80f5744c
Your Name <you@example.com>
2020-03-02 16:44:24 -0300

    Criando servidor com Engine, nunjucks

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const cor = "branco"
+/*const cor = "branco"
 const tamanho = 2.5
 
 function  verificaCopo() {
@@ -12,3 +12,25 @@ const copo = {
 }
 
 console.log(copo)
+*/
+//Configurando o servidor.
+const express = require("express")
+const server = express()
+
+//Configurar o servidor para acessar arquivos staticos
+server.use(express.static('public'))
+
+//configurando a template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("./", {
+    express: server
+})
+
+//Configurando apresentação da pagina
+server.get("/", function(req, res){
+    return res.render("index.html")
+})
+//Ligando o servidor e permitindo acesso a porta 3000
+server.listen(3000, function(){
+    console.log("Iniciei o servidor.")
+})
